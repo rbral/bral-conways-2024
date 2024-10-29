6a567ff42f16eda02fbba309923f1fb8a8deaa13
rbral <rbral@student.touro.edu>
2024-10-29 13:10:27 -0400

    togglecellOFF test added and passes test

--- src/test/java/bral/conways/GridControllerTest.java
+++ src/test/java/bral/conways/GridControllerTest.java
@@ -19,8 +19,6 @@ public class GridControllerTest
         GridComponent view = mock();
         GridController controller = new GridController(model, view);
         doReturn(5).when(view).getCellSize();
-        /*doReturn(100).when(model).getWidth();
-        doReturn(100).when(model).getHeight();*/
         doReturn(100).when(view).getWidth();
         doReturn(529).when(view).getHeight(); // that's the height that the screen opens with
 
@@ -61,7 +59,23 @@ public class GridControllerTest
     @Test
     void toggleCellOff()
     {
+        // given
+        Grid model = mock();
+        GridComponent view = mock();
+        GridController controller = new GridController(model, view);
+        doReturn(5).when(view).getCellSize();
+        doReturn(100).when(view).getWidth();
+        doReturn(529).when(view).getHeight(); // that's the height that the screen opens with
 
+        doReturn(true).when(model).isAlive(10, 85);
+        doReturn(true).when(model).isInBounds(10, 85);
+
+        // when
+        controller.toggleCell(50, 100);
+
+        // then
+        verify(model).remove(10, 85);
+        verify(view).repaint();
     }
 
 
